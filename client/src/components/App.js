691f9b233cf2c4cc18e915a5b09a53d5a886f9d3
suaib <suaib8211@gmail.com>
2023-10-21 19:09:07 +0530

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/App.css";
+import "../styles/bootstrap.min.css";
+import BookMyShow from "./BookMyShow";
+
+
+const App = () => {
+  return (
+    <div className="container">
+      <BookMyShow /> {/*  Displaying BookmyShow Page  */}
+    </div>
+  );
+};
+
+export default App;
